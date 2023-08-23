56130ea99843625c19c8e0c3ec0a42d843a71772
wwc129 <wangwenchao.wwc@alibaba-inc.com>
2023-08-23 23:08:38 +0800

    fix

--- build/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/wwc129/cpp_tools/build/_deps/googletest-src/googletest
-# Build directory: /Users/wwc129/cpp_tools/build/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/wwc129/cpp_tools/tests
-# Build directory: /Users/wwc129/cpp_tools/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/wwc129/cpp_tools/build/tests/all_test[1]_include.cmake")
-subdirs("../_deps/googletest-build")
