c8adad8e6bc4e996c59049767a6cf7f1c6c1d924
wwc129 <wangwenchao.wwc@alibaba-inc.com>
2023-12-28 22:52:46 +0800

    stopwatch header only

--- src/stopwatch.hpp
+++ src/stopwatch.hpp
@@ -5,29 +5,49 @@
 
 namespace wwc {
     class stopwatch {
-      public:
-        stopwatch(bool start=false);
+    public:
+        stopwatch(bool start = false) : running_{start}, prev_{std::chrono::steady_clock::now()} {}
         stopwatch(const stopwatch& other) = delete;
         stopwatch(stopwatch&& other) = delete;
         stopwatch& operator=(const stopwatch& other) = delete;
-        
-        auto now() const noexcept {
-            return std::chrono::steady_clock::now();
+
+        auto now() const noexcept { return std::chrono::steady_clock::now(); }
+        void reset() noexcept {
+            running_ = false;
+            prev_ = now();
+            passed_ = std::chrono::nanoseconds{0};
         }
-        void reset() noexcept;
-        bool start() noexcept;
-        bool stop() noexcept;
-        bool is_running() const noexcept {
-            return running_;
+        bool start() noexcept {
+            if (running_) {
+                return false;
+            }
+            running_ = true;
+            return true;
         }
-        auto tell() const noexcept ;
-        unsigned long tell_secs() const noexcept;
-        unsigned long tell_mills() const noexcept;
-        unsigned long tell_micros() const noexcept;
-        
-      private:
+        bool stop() noexcept {
+            if (!running_) {
+                return false;
+            }
+            running_ = false;
+            passed_ += (now() - prev_);
+            prev_ = now();
+            return true;
+        }
+        bool is_running() const noexcept { return running_; }
+        auto tell() const noexcept {
+            if (running_) {
+                return passed_ + (now() - prev_);
+            }
+            return passed_;
+        }
+
+        unsigned long tell_secs() const noexcept { return std::chrono::duration_cast<std::chrono::seconds>(tell()).count(); }
+        unsigned long tell_mills() const noexcept { return std::chrono::duration_cast<std::chrono::milliseconds>(tell()).count(); }
+        unsigned long tell_micros() const noexcept { return std::chrono::duration_cast<std::chrono::microseconds>(tell()).count(); }
+
+    private:
         std::chrono::time_point<std::chrono::steady_clock, std::chrono::nanoseconds> prev_;
-        std::chrono::nanoseconds passed_ {0};
+        std::chrono::nanoseconds passed_{0};
         bool running_ = false;
     };
 }  // namespace wwc
