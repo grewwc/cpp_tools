40b17fb681cf024dcb56f82a0c442584b9e513c1
wwc129 <wangwenchao.wwc@alibaba-inc.com>
2024-05-13 14:46:40 +0800

    print format

--- NormalUtils.hpp
+++ NormalUtils.hpp
@@ -4,8 +4,9 @@
 
 #include "RandomUtils.hpp"
 #include "Types.hpp"
-#include "src/algorithm.hpp"
 #include "src/String.hpp"
+#include "src/algorithm.hpp"
+#include "src/argparser.hpp"
 #include "src/barrier.hpp"
 #include "src/countdown_latch.hpp"
 #include "src/download_utils.hpp"
@@ -13,10 +14,10 @@
 #include "src/jthread.hpp"
 #include "src/mdc.hpp"
 #include "src/num_utils.hpp"
-#include "src/argparser.hpp"
 
 namespace wwc {
-    template <typename T, template <typename, typename...> class Container, typename = std::enable_if_t<!std::is_pointer_v<std::decay_t<T>>>>
+    template <typename T, template <typename, typename...> class Container,
+              typename = std::enable_if_t<!std::is_pointer_v<std::decay_t<T>>>>
     Container<const T *> to_pointer_container(const Container<T> &container) {
         Container<const T *> result;
         result.reserve(container.size());
@@ -25,20 +26,20 @@ namespace wwc {
         return result;
     }
 
-    template <typename T, template <typename, typename...> class Container, typename = std::enable_if_t<!std::is_pointer_v<std::decay_t<T>>>>
+    template <typename T, template <typename, typename...> class Container,
+              typename = std::enable_if_t<!std::is_pointer_v<std::decay_t<T>>>>
     Container<T *> to_pointer_container(Container<T> &container) {
         Container<T *> result;
         result.reserve(container.size());
-        std::transform(std::begin(container), std::end(container), std::back_inserter<Container<T *>>(result), [](T &val) { return &val; });
+        std::transform(std::begin(container), std::end(container), std::back_inserter<Container<T *>>(result),
+                       [](T &val) { return &val; });
         return result;
     }
 
     template <typename FirstArg, typename... Args>
     void print(FirstArg &&first, Args &&...args) {
-        std::cout << "[" << std::forward<FirstArg>(first) << "]";
-        ((std::cout << ", "
-                    << "[" << std::forward<Args>(args) << "]"),
-         ...);
+        std::cout << std::forward<FirstArg>(first);
+        ((std::cout << " " << std::forward<Args>(args)), ...);
         std::cout << std::endl;
     }
 
