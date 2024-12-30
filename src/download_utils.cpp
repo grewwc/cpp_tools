78b4c76d1d0a5f3c0e83994a2dda141a984a419c
之达 <wangwenchao.wwc@alibaba-inc.com>
2024-12-30 15:29:20 +0800

    bugfix

--- src/download_utils.cpp
+++ src/download_utils.cpp
@@ -1,8 +1,7 @@
 #include "download_utils.hpp"
 #include "normal_utils.hpp"
 
-static std::once_flag init_flag;
-static std::once_flag cleanup_flag;
+
 namespace wwc {
     bool download_url(const char* url, std::string& chunk) {
         try {
